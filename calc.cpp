17e15856cdf13242fb9b9df0705a00d49c1d1360
VGA <74012856+VGAeater@users.noreply.github.com>
2024-09-25 15:59:09 -0400

    Update calc.cpp

--- calc.cpp
+++ calc.cpp
@@ -1,14 +1,23 @@
+/*
+Name: Stratton
+Program: calculator
+date: 9/25
+Extra: calc is in one line
+*/
+
 #include<iostream>
 #include<cmath>
 using namespace std;
 
 int main()
 {
+    // set
     double num1, num2;
     cout << "GIMME NUMBER" << endl;
     cin >> num1;
     cout << "GIMME NUMBER" << endl;
     cin >> num2;
 
+    // calculate
     cout << num1 + num2 << endl << num1 - num2 << endl << num1 * num2 << endl << num1 / num2 << endl << fmod(num1, num2) << endl << round(num1*100)/100 << endl << ++num1 << endl << --num2 << endl;<M-C-F3>
 }
