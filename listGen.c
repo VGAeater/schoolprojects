1fa22520a411e189339de0686e4bba8eaed02929
VGAeater <stratjel@gmail.com>
2024-11-11 10:32:23 -0500

    hi

---
+++ listGen.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+struct entry {
+	char *string;
+	size_t size;
+	struct entry *next;
+};
+
+int main() {
+	struct entry first;
+	struct entry *curr = &first;
+	while (1) {
+		printf("write something:\n");
+		getline(&curr->string, &curr->size, stdin);
+		printf("go again? ");
+		if (getc(stdin) != 'y')
+			break;
+		while (getc(stdin) != '\n') {}
+		struct entry *next = malloc(sizeof(struct entry*));
+		curr->next = next;
+		curr = next;
+	}
+
+	printf("\n\n");
+	curr = &first;
+	while (1) {
+		printf("%s", curr->string);
+		curr = curr->next;
+		if (curr == NULL)
+			break;
+	}
+}
