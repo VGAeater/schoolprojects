e524e23518a73a240cc2b08205ec575721933dfb
VGAeater <stratjel@gmail.com>
2024-11-21 10:44:51 -0500

    aafda

--- functions.c
+++ functions.c
@@ -26,8 +26,8 @@ int main() {
 	printf("WHat is ur name: ");
 	char name[20];
 	scanf("%s", name);
-	customGreet(name, getFavoriteNumber());
 	int favNumber = getFavoriteNumber();
+	customGreet(name, favNumber);
 	int a, b, c;
 	printf("enter 3 nums: ");
 	scanf("%d %d %d", &a, &b, &c);
